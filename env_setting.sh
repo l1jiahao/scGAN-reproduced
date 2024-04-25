9c12dc491272186593f8df762984ec135d12dd31
Jiahao Li <argon_cs@163.com>
2024-04-25 11:14:34 +0800

    :fire: 添加 conda 安装环境的脚本

---
+++ env_setting.sh
@@ -0,0 +1,9 @@
+# 按照以下步骤准备环境, 此脚本适用于 RTX 2080Ti: NVIDIA-SMI 525.116.04   Driver Version: 525.116.04   CUDA Version: 12.0 
+conda create -n scGAN python=3.5
+conda activate scGAN
+conda install cudatoolkit=9.0
+conda install cudnn=7
+conda install cmake=3.18.2
+pip install --upgrade pip
+# 注释 tables package
+pip install -r requirements.txt
